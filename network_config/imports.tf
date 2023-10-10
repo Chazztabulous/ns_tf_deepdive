815aa360831e651996f46ef6dbcc58bfc2917499
Nick Stricklett <nstricklett@costco.com>
2023-10-10 16:52:06 -0700

    fixed

--- network_config/imports.tf
+++ network_config/imports.tf
@@ -37,12 +37,12 @@ import {
   id = "subnet-03f7796e6819b67ba/rtb-05c269505c4426d48" #PublicSubnet1/PublicRouteTable
 }
 
-# import {
-#   to = module.main.aws_route_table_association.public[1]
-#   id = "subnet-0609a6c62eb8644b1/rtb-05c269505c4426d48" #PublicSubnet2/PublicRouteTable
-# }
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-0609a6c62eb8644b1/rtb-05c269505c4426d48" #PublicSubnet2/PublicRouteTable
+}
 
-# import {
-#   to = aws_security_group.ingress
-#   id = "" #NoIngressSecurityGroup
-# }
+import {
+  to = aws_security_group.ingress
+  id = "sg-0bfca262797620ceb" #NoIngressSecurityGroup
+}
