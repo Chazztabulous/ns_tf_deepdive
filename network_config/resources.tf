a25515e6a995386486488b2d231be9ce9332bc69
Nick Stricklett <nstricklett@costco.com>
2023-10-10 16:58:36 -0700

    tagging testing

--- network_config/resources.tf
+++ network_config/resources.tf
@@ -17,7 +17,7 @@ data "aws_availability_zones" "available" {}
 ##################################################################################
 locals {
   common_tags = {
-    
+    Environment = var.environment
   }
 }
 
@@ -39,4 +39,26 @@ module "main" {
   enable_nat_gateway = false
 
   tags = local.common_tags
+}
+
+resource "aws_security_group" "ingress" {
+  description = "Security group with no ingress rule"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress                = []
+  name                   = "no-ingress-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = local.common_tags
+  tags_all               = {}
+  vpc_id                 = module.main.vpc_id
 }
